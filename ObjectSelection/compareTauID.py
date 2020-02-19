42b43e61b8ba02efe9727223f6221bcc28f257ed
Liam <liamwezenbeek@hotmail.com>
2020-02-19 18:25:19 +0100

    compareTauID can now also calculate the efficiency of just reconstructing a tau without ID and combining reco and ID

--- ObjectSelection/compareTauID.py
+++ ObjectSelection/compareTauID.py
@@ -18,6 +18,8 @@ argParser.add_argument('--sample',   action='store',      default=None,   help='
 argParser.add_argument('--isTest',   action='store_true', default=False,  help='Run a small test')
 argParser.add_argument('--runLocal', action='store_true', default=False,  help='use local resources instead of Cream02')
 argParser.add_argument('--dryRun',   action='store_true', default=False,  help='do not launch subjobs, only show them')
+argParser.add_argument('--includeReco',   action='store_true', default=False,  help='look at the efficiency for a gen tau to be both reconstructed and identified. Currently just fills the efficiency for isolation')
+argParser.add_argument('--onlyReco',   action='store_true', default=False,  help='look at the efficiency for a gen tau to be reconstructed. Currently just fills the efficiency for isolation')
 args = argParser.parse_args()
 
 #
@@ -107,10 +109,14 @@ def allowedGenstatus(isoAlgo, eleAlgo, muAlgo):
 #Set output dir
 #
 from HNL.Tools.helpers import makeDirIfNeeded
-output_name = os.path.join(os.getcwd(), 'data', os.path.basename(__file__).split('.')[0], sample.output)
+output_name = os.path.join(os.getcwd(), 'data', os.path.basename(__file__).split('.')[0])
+if args.includeReco: output_name = os.path.join(output_name, 'includeReco')
+if args.onlyReco: output_name = os.path.join(output_name, 'onlyReco')
+output_name = os.path.join(output_name, sample.output)
 if args.isChild:
     output_name += '/tmp_'+sample.output
 
+print output_name
 
 print 'Getting things ready to start the event loop'
 
@@ -127,53 +133,113 @@ def getWP(algorithm):
 
 #Initialize histograms
 from HNL.Tools.ROC import ROC
+from HNL.Tools.efficiency import Efficiency
 list_of_roc = []
+list_of_var_hist = {'efficiency' : {}, 'fakerate' : {}}
 for algo in full_list:
 
     algo_wp = getWP(algo) 
  
     algo_bin = np.arange(0., len(algo_wp)+1., 1)
-    var = lambda c, i : c.var[i]
+    var_roc = lambda c, i : c.var_roc[i]
     
     name = algo[0] + '-' + algo[1] + '-' + algo[2]
-    tot_name = output_name+'/'+ sample.name + '_'+name+ '_' + args.subJob+'.root'
+    tot_name = output_name+'/'+ sample.name + '_'+name+'-ROC_' + args.subJob+'.root'
     makeDirIfNeeded(tot_name)
-    list_of_roc.append(ROC(name, var, 'tmp', tot_name, algo_bin))   #TODO: Currently the way the var is implemented in Histogram class makes this a little cumbersome, change this
+    list_of_roc.append(ROC(name, var_roc, 'tmp', tot_name, algo_bin))   #TODO: Currently the way the var is implemented in Histogram class makes this a little cumbersome, change this
+
+    #Efficiency histograms for variables
+    var_hist = {'pt': [lambda c : c.pt_tau, np.arange(0., 200., 10.),  ('p_T^{#tau}(offline) [GeV]' , 'Efficiency')],
+                'eta': [lambda c : c.eta_tau, np.arange(-2.5, 2.5, 0.5), ('#eta^{#tau}(offline) [GeV]' , 'Efficiency')]}
+    
+    list_of_var_hist['efficiency'][algo] = {}
+    list_of_var_hist['fakerate'][algo] = {}
+    print list_of_var_hist['fakerate'].keys()
+    for v in var_hist.keys():
+        list_of_var_hist['efficiency'][algo][v] = {}
+        list_of_var_hist['fakerate'][algo][v] = {}
+        for wp in algo_wp:
+            tot_name = output_name+'/'+ sample.name + '_efficiency_' + args.subJob+'.root'
+            list_of_var_hist['efficiency'][algo][v][wp] = Efficiency('efficiency_'+v, var_hist[v][0], var_hist[v][2], tot_name, var_hist[v][1])
+            tot_name = output_name+'/'+ sample.name + '_fakerate_' + args.subJob+'.root'
+            list_of_var_hist['fakerate'][algo][v][wp] = Efficiency('fakerate_'+v, var_hist[v][0], var_hist[v][2], tot_name, var_hist[v][1], subdirs = [algo[0] + '-' + algo[1] + '-' + algo[2], 'fakerate_'+v])
 
 #Determine if testrun so it doesn't need to calculate the number of events in the getEventRange
 if args.isTest:
-    event_range = xrange(1000)
+    event_range = xrange(150)
 else:
     event_range = sample.getEventRange(int(args.subJob))
 
-chain.var = np.arange(0.5, getHighestLen()+0.5, 1)
+chain.var_roc = np.arange(0.5, getHighestLen()+0.5, 1)
 
 from HNL.Tools.helpers import progress
 from HNL.ObjectSelection.tauSelector import isGeneralTau
+from HNL.ObjectSelection.tauSelector import isGoodGenTau, matchGenToReco
+from HNL.EventSelection.eventCategorization import EventCategory
+from HNL.EventSelection.eventSelection import select3GenLeptons
+ec = EventCategory(chain)
 for entry in event_range:
 
     chain.GetEntry(entry)
     progress(entry - event_range[0], len(event_range))
 
-
-    #Loop over real taus for efficiency
-    for lepton in xrange(chain._nLight, chain._nL):
-       
+    #Loop over generator level taus if reco and id are measured
+    if args.includeReco:
+        if not select3GenLeptons(chain, chain): continue
+        chain.pt_tau = chain.l_pt[0]
+        chain.eta_tau = chain.l_eta[0]   
+        matched_l = matchGenToReco(chain, chain.l1)
         for i, algo in enumerate(full_list): 
-            if algo[0] != 'none' and not isGeneralTau(chain, lepton, algo[0], 'none', 'againstElectron', 'none', 'againstMuon', 'none', needDMfinding=False): continue
-            elif algo[0] == 'none' and not isGeneralTau(chain, lepton, algo[0], 'loose', 'againstElectron', 'none', 'againstMuon', 'none', needDMfinding=True): continue
+            for index, wp in enumerate(getWP(algo)):
+                passed = False
+                if args.onlyReco: 
+                    passed=matched_l is not None
+                else:
+                    passed = matched_l is not None and isGeneralTau(chain, matched_l, algo[0], wp, algo[1], wp, algo[2], wp)
+                list_of_roc[i].fillEfficiency(chain, index, passed)
+                for v in list_of_var_hist['efficiency'][algo].keys():
+                    list_of_var_hist['efficiency'][algo][v][wp].fill(chain, 1., passed)
 
-            if chain._tauGenStatus[lepton] == 5 and not isBkgr: 
+#        for lepton in xrange(chain._gen_nL):
+#            chain.pt_tau = chain._gen_lPt[lepton]
+#            chain.eta_tau = chain._gen_lEta[lepton]   
+#            if not isGoodGenTau(chain, lepton):    continue
+#            matched_l = matchGenToReco(chain, lepton)
+#            for i, algo in enumerate(full_list): 
+#                for index, wp in enumerate(getWP(algo)):
+#                    passed = False
+#                    if args.onlyReco: 
+#                        passed=matched_l is not None
+#                    else:
+#                        passed = matched_l is not None and isGeneralTau(chain, matched_l, algo[0], wp, algo[1], wp, algo[2], wp)
+#                    list_of_roc[i].fillEfficiency(chain, index, passed)
+#                    for v in list_of_var_hist['efficiency'][algo].keys():
+#                        list_of_var_hist['efficiency'][algo][v][wp].fill(chain, 1., passed)
+
+    else:
+        #Loop over real taus for efficiency
+        for lepton in xrange(chain._nLight, chain._nL):
             
-                for index, wp in enumerate(getWP(algo)):
-                    passed = isGeneralTau(chain, lepton, algo[0], wp, algo[1], wp, algo[2], wp)
-                    list_of_roc[i].fillEfficiency(chain, index, passed)
+            chain.pt_tau = chain._lPt[lepton]
+            chain.eta_tau = chain._lEta[lepton]   
+        
+            for i, algo in enumerate(full_list): 
+                if algo[0] != 'none' and not isGeneralTau(chain, lepton, algo[0], 'none', 'againstElectron', 'none', 'againstMuon', 'none', needDMfinding=False): continue
+                elif algo[0] == 'none' and not isGeneralTau(chain, lepton, algo[0], 'loose', 'againstElectron', 'none', 'againstMuon', 'none', needDMfinding=True): continue
 
+                if chain._tauGenStatus[lepton] == 5: 
+                    for index, wp in enumerate(getWP(algo)):
+                        passed = isGeneralTau(chain, lepton, algo[0], wp, algo[1], wp, algo[2], wp)
+                        list_of_roc[i].fillEfficiency(chain, index, passed)
+                        for v in list_of_var_hist['efficiency'][algo].keys():
+                            list_of_var_hist['efficiency'][algo][v][wp].fill(chain, 1., passed)
 
-            elif isBkgr and chain._tauGenStatus[lepton] in allowedGenstatus(algo[0], algo[1], algo[2]):
-                for index, wp in enumerate(getWP(algo)):
-                    passed = isGeneralTau(chain, lepton, algo[0], wp, algo[1], wp, algo[2], wp)
-                    list_of_roc[i].fillMisid(chain, index, passed)
+                elif chain._tauGenStatus[lepton] in allowedGenstatus(algo[0], algo[1], algo[2]):
+                    for index, wp in enumerate(getWP(algo)):
+                        passed = isGeneralTau(chain, lepton, algo[0], wp, algo[1], wp, algo[2], wp)
+                        list_of_roc[i].fillMisid(chain, index, passed)
+                        for v in list_of_var_hist['fakerate'][algo].keys():
+                            list_of_var_hist['fakerate'][algo][v][wp].fill(chain, 1., passed)
 
 
 #
@@ -181,3 +247,12 @@ for entry in event_range:
 #
 for r in list_of_roc:
     r.write(True)
+
+for eff in ['efficiency', 'fakerate']:
+    for a, algo in enumerate(full_list): 
+        for i, v in enumerate(list_of_var_hist['fakerate'][algo].keys()):
+            for j, wp in enumerate(list_of_var_hist['fakerate'][algo][v].keys()):
+                if a == 0 and i == 0 and j == 0:
+                    list_of_var_hist[eff][algo][v][wp].write(append = False, subdirs = [algo[0] + '-' + algo[1] + '-' + algo[2] + '-' + wp, eff+'_'+v])
+                else:
+                    list_of_var_hist[eff][algo][v][wp].write(append = True, subdirs = [algo[0] + '-' + algo[1] + '-' + algo[2] + '-' + wp, eff+'_'+v]) 
--- ObjectSelection/plotTauID.py
+++ ObjectSelection/plotTauID.py
@@ -4,13 +4,6 @@ import os
 import glob
 from HNL.Tools.helpers import makePathTimeStamped
 
-
-#Merges subfiles if needed
-merge_files = glob.glob(os.getcwd()+'/data/compareTauID/*')
-for mf in merge_files:
-    if "Results" in mf: continue
-    merge(mf)
-
 #
 # Argument parser and logging
 #
@@ -19,6 +12,13 @@ argParser = argparse.ArgumentParser(description = "Argument parser")
 argParser.add_argument('algo',     action='store',      default=None,   help='Select algorithms', choices=['iso', 'eleDiscr', 'muDiscr'])
 argParser.add_argument('bkgr',     action='store',      default=None,   help='Select bkgr')
 args = argParser.parse_args()
+
+
+#Merges subfiles if needed
+merge_files = glob.glob(os.getcwd()+'/data/compareTauID/*')
+for mf in merge_files:
+    if "Results" in mf: continue
+    merge(mf)
 
 if args.algo == 'iso':
     input_name = '*-none-none.root'
@@ -40,15 +40,43 @@ output_dir = makePathTimeStamped(os.getcwd()+'/data/Results/compareTauID/'+args.
 for sample in samples:
     print 'plotting', sample
     curves = []
+    ordered_f_names = []
     for f_name in f_names:
         keyNames = []
+        ordered_f_names.append(f_name)
         sub_files = glob.glob(os.getcwd()+'/data/compareTauID/'+sample+'/'+f_name+'.root')
         for f in sub_files:
             roc_curve = ROC(f_name, 1., '', f, misid_path =bkgr_base + '/'+f_name+'.root')
             curves.append(roc_curve.returnGraph())
-    p = Plot(curves, f_names, sample, 'efficiency', 'misid', y_log=True)
+    p = Plot(curves, ordered_f_names, sample, 'efficiency', 'misid', y_log=True)
     p.drawGraph(output_dir = output_dir)
+       
+from HNL.Tools.efficiency import Efficiency
+from HNL.Tools.helpers import rootFileContent
+from ROOT import TFile
+var = ['pt', 'eta']
+inputFiles = {'efficiency' : glob.glob(os.getcwd()+'/data/compareTauID/*/*efficiency*'), 'fakerate': glob.glob(os.getcwd()+'/data/compareTauID/*/*fakerate*')}
+for eff in ['efficiency', 'fakerate']:
+    for f in inputFiles[eff]:
+        rf = TFile(f)
+        key_names = [k[0] for k in rootFileContent(rf)]
+        filtered_key_names = {fk.rsplit('-', 1)[0] for fk in key_names}
+        for fk in filtered_key_names:
+            sample = f.split('/')[-2]
+            fk_name = fk.split('/')[1]
+            wp_list = []
+            efficiency = []
+            for k in key_names:
+                if not fk_name in k: continue
+                wp = k.rsplit('-', 1)[-1]
+                wp_list.append(wp)
+                for v in var:
+                    print f, v
+                    efficiency.append(Efficiency(eff+'_'+v, None, None, f, subdirs = [k, eff+'_'+v]))
+            p = Plot([eff.getEfficiency() for eff in efficiency], wp_list)
+            p.drawHist(output_dir = os.getcwd()+'/data/Results/compareTauID/'+args.algo+'/var/'+sample, draw_option = 'Hist')
         
+ 
 #graph = roc_curve.return_graph()
 #import ROOT
 #c = ROOT.TCanvas('x', 'x')
