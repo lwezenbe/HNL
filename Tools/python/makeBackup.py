1fef8ee30c582c5bc110a1ac240f9e0f3ca703cf
Liam <liamwezenbeek@hotmail.com>
2021-02-19 15:25:14 +0100

    Add pdf and png files to backup

--- Tools/python/makeBackup.py
+++ Tools/python/makeBackup.py
@@ -1,17 +1,18 @@
 from HNL.Tools.helpers import makeDirIfNeeded, makePathTimeStamped
 import os
 
-skipDirs  = ['documents', 'log', '.git', 'virenv', 'utils', 'Results']
+skipDirs  = ['documents', 'log', '.git', 'virenv', 'utils']
 skipFiles = []
+extensions_to_copy = ['root', 'pdf', 'png']
 
 file_paths = []
 base_path = os.path.join(os.path.expandvars('$CMSSW_BASE'), 'src', 'HNL')
 for root, dirs, files in os.walk(base_path, topdown=True):
     dirs[:]  = [d for d in dirs  if d not in skipDirs]
-    root_files = [os.path.join(root, f) for f in files if f[-5:] == '.root' and f not in skipFiles]
+    root_files = [os.path.join(root, f) for f in files if f.split('.')[-1] in extensions_to_copy and f not in skipFiles]
     if len(root_files) != 0:
-        file_paths.append(os.path.join(root, '*.root'))
-
+        for ext in extensions_to_copy:
+            file_paths.append(os.path.join(root, '*.'+ext))
 
 cmssw_version = os.path.expandvars('$CMSSW_BASE').rsplit('/', 1)[-1]
 central_destination =  makePathTimeStamped('/user/lwezenbe/private/Backup/'+cmssw_version+'/AllOutput')
