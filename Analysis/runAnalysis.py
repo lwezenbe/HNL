414bfa676f1632e1e52b2d62ccb4ea66fd402bd4
Liam <liamwezenbeek@hotmail.com>
2022-01-19 11:26:27 +0100

    Remove remnant from testing

--- Analysis/runAnalysis.py
+++ Analysis/runAnalysis.py
@@ -274,8 +274,7 @@ if not args.makePlots and not args.makeDataCards:
         for entry in event_range:
             
             chain.GetEntry(entry)
-            # if args.isTest: progress(entry - event_range[0], len(event_range))
-            progress(entry - event_range[0], len(event_range))
+            if args.isTest: progress(entry - event_range[0], len(event_range))
  
             cutter.cut(True, 'Total')
 
